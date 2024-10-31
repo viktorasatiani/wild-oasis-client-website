36831d57040b062d6038f99aa65fabcfc3f53fd8
viktorasatiani <vikaasatiani@yahoo.com>
2024-10-31 19:39:28 +0400

    used suspense

---
+++ app/_components/CabinList.js
@@ -0,0 +1,16 @@
+import { getCabins } from "../_lib/data-service";
+import CabinCard from "./CabinCard";
+
+async function CabinList() {
+  const cabins = await getCabins();
+  console.log(cabins);
+  return (
+    <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:gap-12 xl:gap-14">
+      {cabins.map((cabin) => (
+        <CabinCard cabin={cabin} key={cabin.id} />
+      ))}
+    </div>
+  );
+}
+
+export default CabinList;
