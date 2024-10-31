381175548bf001e9775bb82fcc4c2c2cc1b5241d
viktorasatiani <vikaasatiani@yahoo.com>
2024-10-31 10:43:08 +0400

    builded home page

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,15 +1,17 @@
+import Image from 'next/image';
 import Link from 'next/link';
-
+import logo from '@/public/logo.png';
 function Logo() {
   return (
     <Link
       href='/'
       className='flex items-center gap-4 z-10'
     >
-      <img
-        src='logo.png'
+      <Image
+        src={logo}
         height='60'
         width='60'
+        quality={80}
         alt='The Wild Oasis logo'
       />
       <span className='text-xl font-semibold text-primary-100'>
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,15 @@
 import Logo from './_components/Logo';
 import Navigation from './_components/Navigation';
+
+import { Josefin_Sans } from 'next/font/google';
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 import '@/app/_styles/globals.css';
+import Header from './_components/Header';
 export const metadata = {
   title: {
     template: '%s | The Wild Oasis',
@@ -13,13 +22,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className='bg-primary-950 text-primary-100 min-h-screen'>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        {children}
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className='flex-1 px-8 py-12'>
+          <main className='max-w-7xl mx-auto'>{children}</main>
+        </div>
       </body>
     </html>
   );
